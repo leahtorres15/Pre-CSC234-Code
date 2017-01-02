3cac5a81334046eb7f634b81b88986dc5434fd53
Leah Torres <leahtorres15@gmail.com>
2017-01-02 14:09:04 -0800

    Made program: Assignment4

--- Assignment4.c
+++ Assignment4.c
@@ -1,10 +1,41 @@
 //WTF does this thing do???
-//Currently it returns 0 :p
 
 #include <stdio.h>
 
 int main() {
-    
+   
+    //
+    int array[10];
+
+    //Use for values in "array"
+    int i=1;
+
+    //
+    int counter=0;
+
+    //Defines values in "array"
+    while (counter < 10) {
+        
+        //
+        i=(2*i)+4;
+       
+        //
+        if (!(i%2)) {
+
+            //
+            array[9-counter]=i;
+           
+            //
+            counter++;
+            }
+    }
+
+    //Sets a for loop with parameter j to get values for "array"
+    for(int j=0; j<10; j++) {
+        
+        //
+        printf("array[%i] = %i\n", j, array[j]);
+    }
 
     return 0;
 }
