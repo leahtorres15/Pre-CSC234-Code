e2fa6847413eb2b8a990d88dcb7624550807ab41
Leah Torres <leahtorres15@gmail.com>
2017-01-07 16:49:21 -0800

    Added completed Assignment5

--- Assignment5.c
+++ Assignment5.c
@@ -7,9 +7,16 @@ float rectangleArea(float length, float width);
 float circleArea(float length, float width);
 
 int main() {
-    float length = x;
-    float width = y;
-    printf("%f\n",float circleArea(float x, float y) - float rectangleArea(float x, float y));
+    float x=2;
+    float y=15;
+
+    for(int i=0; i<11; i++){
+        float len = x+(2*i);
+        float wid = y-(.3*i);
+        float diff = circleArea(len, wid) - rectangleArea(len, wid);
+        printf("Difference in areas using length %f and width %f: %f\n", 
+                len, wid, diff);
+    }
     return 0;
 }
 
@@ -23,3 +30,5 @@ float circleArea(float length, float width){
     float radius = sqrt((pow(x,2)) + (pow(y,2)));
     return (radius*radius* M_PI);
 }
+
+
